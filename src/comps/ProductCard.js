19ad4673a713f9fba5d329f36c66e87fad9ea4d4
mohamedtarek77 <mtarek7878@gmail.com>
2024-08-26 21:24:39 +0400

    02

--- src/comps/ProductCard.js
+++ src/comps/ProductCard.js
@@ -5,7 +5,7 @@ const ProductCard = ({ content, name, title, img, link }) => {
   return (
     <div className="flex   flex-col  gap-3 sm:justify-between bg-slate-300   px-10 sm:py-12 py-5  rounded-[20px]     md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
       <div className="flex  flex-col justify-center items-center gap-5 sm:mt-0 mt-2">
-        <Image src={img} alt={name} className=" w-40 h-40  rounded-lg" />
+        <Image src={img} alt={name} className=" sm:w-60 w-40 sm:h-60 h-40  rounded-lg" />
       </div>
 
       <div className="flex flex-col  text-secondary gap-5 ">
